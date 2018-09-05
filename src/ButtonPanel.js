e50214b5cde7fb579ac1e7088506aa6cc9b0f2fe
jeunghwansuh <jeunghwansuh@gmail.com>
2018-09-04 20:19:22 -0400

    Added howler.js library

--- src/ButtonPanel.js
+++ src/ButtonPanel.js
@@ -1,23 +1,111 @@
 import React, { Component } from 'react';
-import "./ButtonPanel.css"
+import "./ButtonPanel.css";
+import Pizzicato from 'pizzicato';
+import {Howl, Howler} from 'howler';
+
+
+import logo from './bark.wav';
+import logo2 from './bark.wav';
 
 
 
 
 
 
+
+class Sound {
+
+  constructor() {
+    console.log("hello")
+  }
+
+  init() {
+       console.log("hello")
+
+  }
+
+  return50(){
+    return 50;
+  }
+
+ 
+
+}
+
+    
+var sound;
+var acousticGuitar;
+var thesound;
 class ButtonPanel extends Component {
 
 
+
+
+  state = { counter: 1};
+  
+
     constructor(props){
         super(props);
+        this.init();
+
+     
+
     }
 
+
+    init(){
+    thesound = logo;
+    acousticGuitar = new Pizzicato.Sound(thesound);
+    sound = new Howl({
+  src: [logo]
+});
+
+    }
+
+
+
+    handleClick = () => {
+
+
+        var s = new Sound();
+
+        this.setState(() => ({
+            counter: s.return50()
+         }));
+
+               
+        sound.play();
+
+    };
+
+    componentDidMount() {
+
+
+    }
+
+  componentWillUnmount() {
+
+    }
+
+
+  
+
+
+
 render() {
+
+
+
+ 
+
+
+
     
         return (
             <div id="main_buttons">
-            <button class = "music_button"> Play </button>
+
+
+            <button class = "music_button" onClick={this.handleClick}> {this.state.counter} </button>
             <button class = "music_button"> Pause </button>
             <button class = "music_button"> Stop </button>
 
